67885ae6443b7679b683ab513cabca24301b04c3
Vansh Arya <vansharya1295@gmail.com>
2022-09-24 05:17:25 +0000

    added hello world

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <main>
       React ⚛️ + Vite ⚡ + Replit 🌀
+      Hello world
+      My name is Vansh Arya
+      I am a codeyogi
     </main>
   )
 }
